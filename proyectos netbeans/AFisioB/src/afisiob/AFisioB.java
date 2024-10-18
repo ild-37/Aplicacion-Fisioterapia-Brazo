e693e6c600e0ff8103fbc61204071b451e954e80
dam2 <dam2@a223e10.dc.domenicoscarlatti.es>
2024-10-18 20:23:36 +0200

    Probando git3

--- proyectos netbeans/AFisioB/src/afisiob/AFisioB.java
+++ proyectos netbeans/AFisioB/src/afisiob/AFisioB.java
@@ -16,7 +16,7 @@ public class AFisioB {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Probando...");
-        System.out.println("Probando...");
+        System.out.println("Probando...");System.out.println("Probando...");
     }
     
 }
